1aadd3751d344b61bc565e10960ab2bd4ad31c8d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-04-30 01:34:53 +0200

    ~ update as the behaviour of the experimental lombok FieldDefaults doesn't apply to static fields anymore

--- src/main/java/com/kasisoft/cdi/services/freemarker/internal/Messages.java
+++ src/main/java/com/kasisoft/cdi/services/freemarker/internal/Messages.java
@@ -2,23 +2,19 @@ package com.kasisoft.cdi.services.freemarker.internal;
 
 import com.kasisoft.libs.common.i18n.*;
 
-import lombok.*;
-import lombok.experimental.*;
-
 /**
  * @author daniel.kasmeroglu@kasisoft.net
  */
-@FieldDefaults(level = AccessLevel.PUBLIC)
 public class Messages {
 
   @I18N("The template '%s' could not be processed. Cause: %s")
-  static I18NFormatter   failed_to_process_template;
+  public static I18NFormatter   failed_to_process_template;
   
   @I18N("The template '%s' could not be loaded. Cause: %s")
-  static I18NFormatter   failed_to_load_template;
+  public static I18NFormatter   failed_to_load_template;
   
   @I18N("The template '%s' is either missing or contains an error.")
-  static I18NFormatter   missing_or_invalid_template;
+  public static I18NFormatter   missing_or_invalid_template;
   
   static {
     I18NSupport.initialize( Messages.class );
